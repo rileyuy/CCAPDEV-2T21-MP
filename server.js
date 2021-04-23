1aaea7936a055cdbf9f7d4d8932945c1f362ce5d
rileyuy <uyjustinriley@gmail.com>
2021-04-23 21:12:37 +0800

    Added node and handlebars code

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require ('express');
+const bodyParser = require ('body-parser');
+const exphbs = require ('express-handlebars');
 const dotenv = require ('dotenv');
-
 const app = express();
 
 dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 
-app.listen (port);
+app.use (express.static(__dirname + "/"))
+app.use (bodyParser.json());
+app.use (bodyParser.urlencoded({extended:true}));
+
+app.set ('view engine', "hbs");
+app.engine ('hbs', exphbs ({
+    extname: 'hbs',
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+}));
+
+app.listen (port, ()=> {
+    console.log ('Listening to port number ' + port);
+});
 
 app.get ('/', (req,res) => {
-    res.sendFile ('./views/index.html', {root: __dirname});
+    //res.sendFile ('./views/index.hbs', {root: __dirname});
+    res.render ("main");
 });
 
 app.get ('/login', (req, res) => {
-    res.sendFile ('./views/login.html', {root: __dirname});
+    // res.sendFile ('./views/login.hbs', {root: __dirname});
 });
