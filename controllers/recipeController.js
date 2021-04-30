4bfee6a9e151650db4c1f77a42b70f2d19d21d03
rileyuy <uyjustinriley@gmail.com>
2021-05-01 04:27:00 +0800

    Untested recipe edit, needs to be connected and tested through view account

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,7 +1,43 @@
 const Recipe = require ('../models/recipe');
 
 const edit_recipe = (req, res) => {
+    var recipeId = req.params.id;
+    User.findOne ({_id: recipeId}, function (err, updateRecipe){
+        if (err) {
+            console.log (err)
+            res.send();
+        }
+        else{
+            if (!updateRecipe){ //if recipe is found in database
+                res.send();
+            }
+            else {
+                console.log (req.body);
+                if (req.body.recipeName) {
+                    updateRecipe.recipeName = req.body.recipeName;
+                }
+                
+                if (req.body.recipeIngredients){
+                    updateRecipe.recipeIngredients = req.body.recipeIngredients;
+                }
+                
+                if (req.body.recipeInstructions){
+                    updateRecipe.recipeInstructions = req.body.recipeInstructions;
+                }
 
+                updateRecipe.save (function(errors, updatedRecipe){
+                    if (errors){
+                        console.log (errors)
+                        res.send();
+                    }
+                    else{
+                        console.log (updatedRecipe);
+                        res.redirect ('/');
+                    }
+                })
+            }
+        }
+    })
 }
 
 const delete_recipe = (req, res) => {
