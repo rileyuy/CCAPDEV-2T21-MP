66e55f831b7f2a97d301717094680416c709baf7
rileyuy <uyjustinriley@gmail.com>
2021-05-21 00:13:48 +0800

    fixed formatting

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -25,28 +25,6 @@ const authenticate = async(req, res, next) => {
 
 }
 
-const checkUser = (req, res, next) => {
-    const token = req.cookies.jwt;
-
-    if (token) {
-      jwt.verify(token, jwtsecret , async (err, decodedToken) => {
-        
-        if (err) {
-          res.locals.user = null;
-          next();
-        } else {
-          let user = await User.findById(decodedToken.id);
-          res.locals.user = user;
-          next();
-        }
-      });
-    } else {
-      res.locals.user = null;
-      next();
-    }
-  };
-
 module.exports = {
-    checkUser,
     authenticate
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,13 +5,18 @@ const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post ('/register',  [body ('email', 'Please enter a valid email address.').trim().isEmail(),
-                            body ('password', 'Please enter a password.').trim().not().isEmpty(),
-                            body ('lastName', 'Please enter your last name.').not().isEmpty(),
-                            body ('firstName', 'Please enter your first name.').not().isEmpty()], authController.user_register);
+router.post ('/register',  [
+    body ('email', 'Please enter a valid email address.').trim().isEmail(),
+    body ('password', 'Please enter a password.').trim().not().isEmpty(),
+    body ('lastName', 'Please enter your last name.').not().isEmpty(),
+    body ('firstName', 'Please enter your first name.').not().isEmpty()
+], authController.user_register);
 
-router.post ('/login', [body ('email', 'Please enter a valid email address.').trim().isEmail(),
-                        body ('password', 'Please enter your password.').trim().not().isEmpty()], authController.user_login);
+router.post ('/login', [
+    body ('email', 'Please enter a valid email address.').trim().isEmail(),
+    body ('password', 'Please enter your password.').trim().not().isEmpty()
+], authController.user_login);
+
 router.get ('/login/:type', viewController.login_view);
 router.get ('/logout', authController.user_logout);
 
